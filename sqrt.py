3c24fbef5a88321289e096889e64ddbb5033c241
Ubuntu <ubuntu@ip-172-31-38-137.ec2.internal>
2024-11-11 09:32:17 +0000

    add sqrt.py file

---
+++ sqrt.py
@@ -0,0 +1,20 @@
+import math
+import sys
+
+def calculate_square_root(num):
+   if isinstance(num, str):
+       raise TypeError("Input must not be string")
+   if num < 0:
+       raise ValueError("Input must be non-negative")
+   return math.sqrt(num)
+
+def main():
+    try:
+        a = int(sys.stdin.read().strip())
+        print(f"Square root of {a} is { calculate_square_root(a)}")
+    except ValueError as e:
+        print("Please enter valid integer")
+
+if __name__ == "__main__":
+    main()
+
